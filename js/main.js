23b4c3e3c1242fe75509b9426b1fc7a516149e17
shakeel-nazir <shakeelnazir@MacBook-Pro.local>
2015-03-04 17:43:17 -0500

    Re-did the JS
    - Re-did the JS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
-var $theCircle = $('.circle');
+var $form = $('.form');
+var $name= $('.name');
+var $list= $('.list');
 
 
-var $form = $('form');
-var $input = $('#the-color');
-var $list = $('.list')
+$form.on('submit', function (e) {    
+e.preventDefault();
 
-
-
-
-$form.on('submit', function (eventObject) {
-
-    eventObject.preventDefault();
+   
+    var $li = $('<li>');
+    var $nameH2 = $('<h2>').html($name.val());
+    var button = $<'button class="x">X</button>');
     
-    var $theli= $('<li>').html($input.val() + '<button class="x">x</button>');
-    
-    $theCircle.prepend($theli);
-    $input.val('');
-    
+    $li.append($nameH2);
+    $li.append($button)
+    $list.append($li);
     
 });
-
-    $form.on('click', 'li', function () {
-      $(this).toggleClass('js-line')
-      
-});
-
-$list.on('click', 'button', function () {
-    $(this).parent().remove();
-})
-

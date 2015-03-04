eae52bde9d5c2da4b97e70ba5e0569915197a9b3
shakeel-nazir <shakeelnazir@MacBook-Pro.local>
2015-03-04 16:10:59 -0500

    Add Images
    - Add Images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-h1 { 
-    color: red
+var $theCircle = $('.circle');
+
+
+var $form = $('form');
+var $input = $('#the-color');
+var $list = $('.list')
+
+
+
+
+$form.on('submit', function (eventObject) {
+
+    eventObject.preventDefault();
     
+    var $theli= $('<li>').html($input.val() + '<button class="x">x</button>');
+    
+    $theCircle.prepend($theli);
+    $input.val('');
+    
+    
+});
+
+    $form.on('click', 'li', function () {
+      $(this).toggleClass('js-line')
+      
+});
+
+$list.on('click', 'button', function () {
+    $(this).parent().remove();
+})
+
